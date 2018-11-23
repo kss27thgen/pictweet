90ad765fc8c1c61b612574c98493da21b722e8fb
Oishi Yuikyo <s2008511@gmail.com>
2018-11-23 16:47:13 +0900

    Initial commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,14 @@
+class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.create(comment_params)
+    redirect_to "/tweets/#{@comment.tweet.id}"
+  end
+
+
+  private
+  def comment_params
+    params.permit(:text, :tweet_id).merge(user_id: current_user.id)
+  end
+
+end
---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,56 @@
+class TweetsController < ApplicationController
+
+before_action :move_to_index,except: [:index,:show]
+before_action :set_tweet,only:[:destroy,:edit,:update,:show]
+
+  def index
+    @tweets=Tweet.includes(:user).page(params[:page]).per(5).order("created_at desc")
+  end
+
+  def new
+  end
+
+  def create
+    @tweet = Tweet.create(tweet_params)
+  end
+
+  def destroy
+    @tweet.destroy if @tweet.user_id == current_user.id
+  end
+
+  def edit
+  end
+
+  def update
+    @tweet.update(tweet_params) if @tweet.user_id == current_user.id
+
+  end
+
+  def show
+    @tweet=Tweet.find(params[:id])
+    @comments=@tweet.comments.includes(:user)
+  end
+
+
+
+
+
+  private
+  def tweet_params
+    params.permit(:image, :text).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
+
+
+
+
+
+end
+
